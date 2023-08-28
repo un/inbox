d2c50ce811ac774eb85d5d7e4cde6953141a37a2
McPizza <o@l1.io>
2023-08-28 08:26:33 +0100

    Add drizzle orm

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,1 +1,19 @@
-export * from '@prisma/client';
+import { drizzle } from 'drizzle-orm/planetscale-serverless';
+import { connect } from '@planetscale/database';
+import * as schema from './schema';
+
+// SELF: add support for non planetscale hosting via mysql2 package
+
+// create the connection
+const connection = connect({
+  host: process.env['DB_HOST'],
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD']
+});
+
+const connectionOptions = {
+  logger: process.env.NODE_ENV === 'development' ? true : false,
+  schema
+};
+
+export const db = drizzle(connection, connectionOptions);
