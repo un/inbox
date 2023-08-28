604303733783d16fba9452ace7fc73fabb567cf7
McPizza <o@l1.io>
2023-08-28 18:27:34 +0100

    revert postal puppet

--- packages/postal-puppet/playground/config.ts
+++
@@ -1,8 +0,0 @@
-export function useRuntimeConfig() {
-  return {
-    postalControlPanel: 'cp',
-    postalUrl: 'one.e.uninbox.dev',
-    postalUser: 'mailbridge@l1.io',
-    postalPass: '^a%g8PgMbLiNi^'
-  };
-}
