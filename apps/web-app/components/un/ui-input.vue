fd5f8526b7e54a7175f59e9998bac22a7cdde12c
McPizza <gh@l1.io>
2023-08-30 11:20:28 +0100

    Components: update input + tooltip (remote val)

--- apps/web-app/components/un/ui-input.vue
+++ apps/web-app/components/un/ui-input.vue
@@ -1,6 +1,131 @@
+<script setup lang="ts">
+  import type { ZodTypeAny } from 'zod';
+  import { useVModel } from '@vueuse/core';
+  import { useFocus } from '@vueuse/core';
+  import type { VariantProps } from 'class-variance-authority';
+
+  type InputProps = VariantProps<typeof inputClasses>;
+
+  type Props = {
+    value?: string;
+    label: string;
+    placeholder: string;
+    icon?: string | null;
+    disabled?: boolean;
+    width?: InputProps['width'];
+    locked?: boolean;
+    helper?: string | null;
+    schema?: ZodTypeAny | null;
+    valid?: boolean | 'remote' | null;
+    validationMessage?: string | null;
+    remoteValidation?: boolean | null;
+  };
+
+  const props = withDefaults(defineProps<Props>(), {
+    value: '',
+    icon: null,
+    disabled: false,
+    width: 'fit',
+    locked: false,
+    helper: null,
+    schema: null,
+    valid: null,
+    validationMessage: null,
+    remoteValidation: false
+  });
+
+  const inputClasses = useUtils().cva('', {
+    variants: {
+      width: {
+        full: 'w-full',
+        fit: 'sm:w-72 md:w-80'
+      },
+      color: {
+        default: 'border-sand-7 focus-within:border-sand-8',
+        valid: 'border-green-7 focus-within:border-green-8',
+        invalid: 'border-red-11 focus-within:border-red-8',
+        remote: 'border-orange-11 focus-within:border-orange-8'
+      },
+      disabled: {
+        true: 'cursor-not-allowed',
+        false: 'cursor-text'
+      }
+    }
+  });
+
+  const editModeEnabled = ref(false);
+  const inputField = ref();
+  // const valid = ref(props.valid);
+  const validationMessage = ref(props.validationMessage);
+  const initialValue = unref(props.value);
+
+  // Switch Focus to input field when edit mode is enabled
+  const { focused: inputFocus } = useFocus(inputField);
+  function toggleEditMode() {
+    editModeEnabled.value = !editModeEnabled.value;
+    // we need a small timeout here to allow the input to become enabled again
+    setTimeout(() => {
+      inputFocus.value = !inputFocus.value;
+    }, 10);
+  }
+  // Computed properties
+  const computedDisabled = computed(() => {
+    return props.locked ? !editModeEnabled.value : props.disabled;
+  });
+
+  const computedBorderColor = computed(() => {
+    // If external/remote validation failed and message passed to component, always set to invalid
+    if (validationMessage.value) {
+      valid.value = false;
+      return inputClasses({ color: 'invalid' });
+    }
+    // If no schema is passed, set to default
+    if (!props.schema && !props.remoteValidation)
+      return inputClasses({ color: 'default' });
+    if (!props.schema && props.remoteValidation) {
+      return inputClasses({ color: 'remote' });
+    }
+
+    if (data.value !== initialValue && !valid.value) {
+      return inputClasses({ color: 'invalid' });
+    }
+    if (data.value && valid.value === true) {
+      return inputClasses({ color: 'valid' });
+    }
+    if (data.value && valid.value === 'remote') {
+      return inputClasses({ color: 'remote' });
+    }
+
+    return inputClasses({ color: 'default' });
+  });
+
+  const emit = defineEmits(['update:value', 'update:valid']);
+  const data = useVModel(props, 'value', emit, { passive: true });
+  const valid = useVModel(props, 'valid', emit, { passive: true });
+
+  // Locally Validate input against schema via props
+  watchDebounced(
+    data,
+    async () => {
+      if (props.schema) {
+        const inputValidationResult = props.schema.safeParse(data.value);
+        if (!inputValidationResult.success) {
+          validationMessage.value =
+            inputValidationResult.error.issues[0].message;
+          valid.value = false;
+          return;
+        }
+      }
+      valid.value = props.remoteValidation ? 'remote' : true;
+      validationMessage.value = null;
+    },
+    { debounce: 350, maxWait: 5000 }
+  );
+</script>
+
 <template>
   <div
-    class="w-80 flex flex-col gap-1 leading-4 text-primary-12"
+    class="flex flex-col gap-1 leading-4 text-primary-12"
     :class="inputClasses({ width: props.width })">
     <div class="flex flex-row justify-between gap-4">
       <label
@@ -12,6 +137,11 @@
         v-if="validationMessage"
         class="w-full truncate text-right text-sm text-red-11"
         >{{ validationMessage }}</span
+      >
+      <span
+        v-if="valid === 'remote'"
+        class="w-full truncate text-right text-sm text-orange-11"
+        >Verifying...</span
       >
     </div>
     <div
@@ -53,114 +183,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-  import type { ZodTypeAny } from 'zod';
-  import { useVModel } from '@vueuse/core';
-  import { useFocus } from '@vueuse/core';
-  import type { VariantProps } from 'class-variance-authority';
-
-  type InputProps = VariantProps<typeof inputClasses>;
-
-  type Props = {
-    value?: string;
-    label: string;
-    placeholder: string;
-    icon?: string | null;
-    disabled?: boolean;
-    width?: InputProps['width'];
-    locked?: boolean;
-    helper?: string | null;
-    schema?: ZodTypeAny | null;
-    valid?: boolean | null;
-    validationMessage?: string | null;
-  };
-
-  const props = withDefaults(defineProps<Props>(), {
-    value: '',
-    icon: null,
-    disabled: false,
-    width: 'fit',
-    locked: false,
-    helper: null,
-    schema: null,
-    valid: null,
-    validationMessage: null
-  });
-
-  const inputClasses = useUtils().cva('', {
-    variants: {
-      width: {
-        full: 'w-full',
-        fit: 'w-80'
-      },
-      color: {
-        default: 'border-sand-7 focus-within:border-sand-8',
-        valid: 'border-green-7 focus-within:border-green-8',
-        invalid: 'border-red-11 focus-within:border-red-8'
-      },
-      disabled: {
-        true: 'cursor-not-allowed',
-        false: 'cursor-text'
-      }
-    }
-  });
-
-  const editModeEnabled = ref(false);
-  const inputField = ref();
-  // const valid = ref(props.valid);
-  const validationMessage = ref(props.validationMessage);
-  const initialValue = unref(props.value);
-
-  // Switch Focus to input field when edit mode is enabled
-  const { focused: inputFocus } = useFocus(inputField);
-  function toggleEditMode() {
-    editModeEnabled.value = !editModeEnabled.value;
-    // we need a small timeout here to allow the input to become enabled again
-    setTimeout(() => {
-      inputFocus.value = !inputFocus.value;
-    }, 10);
-  }
-  // Computed properties
-  const computedDisabled = computed(() => {
-    return props.locked ? !editModeEnabled.value : props.disabled;
-  });
-  const computedBorderColor = computed(() => {
-    // If external/remote validation failed and message passed to component, always set to invalid
-    if (validationMessage.value) {
-      valid.value = false;
-      return inputClasses({ color: 'invalid' });
-    }
-    // If no schema is passed, set to default
-    if (!props.schema) return inputClasses({ color: 'default' });
-    if (props.schema) {
-      if (data.value !== initialValue && !valid.value) {
-        return inputClasses({ color: 'invalid' });
-      }
-      if (data.value && valid.value) {
-        return inputClasses({ color: 'valid' });
-      }
-    }
-    return inputClasses({ color: 'default' });
-  });
-
-  const emit = defineEmits(['update:value', 'update:valid']);
-  const data = useVModel(props, 'value', emit, { passive: true });
-  const valid = useVModel(props, 'valid', emit, { passive: true });
-
-  // Locally Validate input against schema via props
-  watchDebounced(
-    data,
-    async () => {
-      if (!props.schema) return;
-      const inputValidationResult = props.schema.safeParse(data.value);
-      if (!inputValidationResult.success) {
-        validationMessage.value = inputValidationResult.error.issues[0].message;
-        return;
-      }
-      valid.value = true;
-      validationMessage.value = null;
-    },
-    { debounce: 350, maxWait: 5000 }
-  );
-</script>
--- apps/web-app/components/un/ui-tooltip.vue
+++ apps/web-app/components/un/ui-tooltip.vue
@@ -2,6 +2,7 @@
   import {
     TooltipArrow,
     TooltipContent,
+    TooltipProvider,
     TooltipRoot,
     TooltipTrigger
   } from 'radix-vue';
@@ -9,25 +10,28 @@
   const toggleState = ref(false);
 
   defineProps({
-    text: { type: String, required: true }
+    text: { type: String, required: true },
+    parentClass: { type: String, required: false }
   });
 </script>
 <template>
-  <TooltipRoot v-model:open="toggleState">
-    <TooltipTrigger class="">
-      <slot />
-    </TooltipTrigger>
-    <Teleport to="body">
-      <TooltipContent
-        :side-offset="5"
-        :collision-padding="32"
-        :avoid-collisions="true"
-        class="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade max-w-80 border border-base-6 rounded bg-base-2 px-4 py-2 text-sm">
-        <div>
-          {{ text }}
-          <TooltipArrow class="fill-base-11" />
-        </div>
-      </TooltipContent>
-    </Teleport>
-  </TooltipRoot>
+  <TooltipProvider>
+    <TooltipRoot v-model:open="toggleState">
+      <TooltipTrigger :class="parentClass">
+        <slot />
+      </TooltipTrigger>
+      <Teleport to="body">
+        <TooltipContent
+          :side-offset="5"
+          :collision-padding="32"
+          :avoid-collisions="true"
+          class="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade max-w-80 border border-base-6 rounded bg-base-2 px-4 py-2 text-sm">
+          <div>
+            {{ text }}
+            <TooltipArrow class="fill-base-11" />
+          </div>
+        </TooltipContent>
+      </Teleport>
+    </TooltipRoot>
+  </TooltipProvider>
 </template>
