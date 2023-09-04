613ab052f556349b916dadd1a7bfa779d781c63d
McPizza <gh@l1.io>
2023-09-04 10:34:11 +0100

    DB: Add note about mysql support

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -3,6 +3,7 @@ import { connect } from '@planetscale/database';
 import * as schema from './schema';
 
 // SELF: add support for non planetscale hosting via mysql2 package
+//! SELF: When adding support for mysql2, we need to find a way to return the Insert IDs. Planetscale driver handles this automatically, but mysql2 dosnt and may need all DB inserts to be modified as a transaction with "LAST_INSERT_ID" being returned.
 
 // create the connection
 const connection = connect({
