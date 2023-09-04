54d4db45071adefa8cff0b581ddbba09bb413bc6
McPizza <gh@l1.io>
2023-09-04 10:35:50 +0100

    WEBAPP: Get Hanko JWKS on build, add to config

--- apps/web-app/server.prepare.ts
+++ apps/web-app/server.prepare.ts
@@ -3,9 +3,18 @@ import { defineNuxtPrepareHandler } from 'nuxt-prepare/config';
 export default defineNuxtPrepareHandler(async () => {
   // Do some async magic here, e.g. fetch data from an API
 
+  // Get and store the Hanko jwks into runtime config
+  const hankoUrl = process.env.WEBAPP_HANKO_API_URL;
+  const hankoJwksResponse = await fetch(
+    `${hankoUrl}/.well-known/jwks.json`
+  ).then(function (response) {
+    return response.json();
+  });
+
   return {
     // Overwrite the runtime config variable `foo`
     runtimeConfig: {
+      hankoJwks: hankoJwksResponse,
       public: {}
     }
   };
