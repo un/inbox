e8b731c1dfb33e5c208553be319ffa87f69bd0d4
McPizza <gh@l1.io>
2023-09-06 22:12:06 +0100

    MAILBRIDGE: ++ verify postal webhook signature

---
+++ apps/mail-bridge/middleware/verifyPostalSig.ts
@@ -0,0 +1,19 @@
+import { validatePostalWebhookSignature } from '../utils/validatePostalWebhookSignature';
+
+export default defineEventHandler(async (event) => {
+  if (getRequestURL(event).pathname.startsWith('/postal')) {
+    const body = await readBody(event);
+    const signature = await getHeader(event, 'x-postal-signature');
+    //TODO: need to support multiple public keys from multiple servers, and return true if any match
+    const publicKey = useRuntimeConfig().postalWebhookPublicKey;
+    const validPostalSignature = await validatePostalWebhookSignature(
+      body,
+      signature,
+      publicKey
+    );
+    if (!validPostalSignature) {
+      sendNoContent(event, 401);
+    }
+    event.context.fromPostal = validPostalSignature;
+  }
+});
