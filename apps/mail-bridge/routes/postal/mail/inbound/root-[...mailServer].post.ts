ca7b4d214099f7754adef0f85dcb0533d4d933e9
McPizza <gh@l1.io>
2023-09-08 14:48:32 +0100

    MAILBRIDGE: Move incoming root http endpoint

--- apps/mail-bridge/routes/postal/mail/inbound/root-[...mailServer].post.ts
+++ apps/mail-bridge/routes/postal/mail/inbound/root-[...mailServer].post.ts
@@ -1,15 +1,14 @@
 /**
- * used for all email messages coming to the root domains
+ * used for all incoming mail from Postal
  */
 
 export default eventHandler((event) => {
   const mailBody = readBody(event);
+  const serverId = event.context.params.mailServer;
 
   //! verify the source of webhook call: https://github.com/postalserver/postal/issues/432#issuecomment-730454586
-  //! extract the to address of the email
-  //! verify it was sent to a root domain
-  //! extract the username from the to email address
-  //! do DB lookup for the addresss
+  // extract event from body
+  // forward event to handler functions
 
   return { status: "I'm Alive 🏝️" };
 });
