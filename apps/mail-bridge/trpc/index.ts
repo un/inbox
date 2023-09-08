cf48d84ff6e938eb07c5abed29026e7de7f37255
McPizza <gh@l1.io>
2023-09-08 14:49:36 +0100

    MB: tRPC change router arch

--- apps/mail-bridge/trpc/index.ts
+++ apps/mail-bridge/trpc/index.ts
@@ -1,11 +1,16 @@
 export * from '@trpc/server';
 import { router } from './trpc';
 import { createContext } from './createContext';
-import { postalPuppetRouter } from './routers/postalPuppetRouter';
+import { orgRouter } from './routers/orgRouter';
+import { emailRoutesRouter } from './routers/emailRoutesRouter';
 
 export const trpcMailBridgeContext = createContext;
+export const trpcMailBridgePostalRouter = router({
+  org: orgRouter,
+  emailRoutes: emailRoutesRouter
+});
 export const trpcMailBridgeRouter = router({
-  postal: postalPuppetRouter
+  postal: trpcMailBridgePostalRouter
 });
 
 export type TrpcMailBridgeRouter = typeof trpcMailBridgeRouter;
