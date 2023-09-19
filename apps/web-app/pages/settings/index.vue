b9c2f9d27cd72d8b431b4c0fdc2bc7c7efd7f72b
McPizza <gh@l1.io>
2023-09-19 17:40:15 +0100

    Web: misc + DEP: update

--- apps/web-app/pages/settings/index.vue
+++ apps/web-app/pages/settings/index.vue
@@ -6,13 +6,6 @@
 <template>
   <div
     class="flex-col flex justify-center gap-2 items-center w-full col-span-2 pl-4 h-full">
-    <div class="col-span-2 pl-4">Select a conversation from the left, or</div>
-    <button
-      class="flex flex-row gap-4 p-2 border-1 rounded w-full items-center justify-center border-base-7 bg-base-3 max-w-80">
-      <icon
-        name="ph-plus"
-        size="20" />
-      <p class="text-sm">Start new</p>
-    </button>
+    <div class="col-span-2 pl-4">Select a category from the left</div>
   </div>
 </template>
