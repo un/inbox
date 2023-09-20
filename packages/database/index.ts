e560c0f6f2ab336dd90be0b2ca6e69eea94342a0
McPizza <gh@l1.io>
2023-09-20 10:51:23 +0100

    DB: update - initial exports read replicas

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -2,11 +2,17 @@ import { drizzle } from 'drizzle-orm/planetscale-serverless';
 import { connect } from '@planetscale/database';
 import * as schema from './schema';
 
-// SELF: add support for non planetscale hosting via mysql2 package
+// SELF: TODO: add support for non planetscale hosting via mysql2 package
 //! SELF: When adding support for mysql2, we need to find a way to return the Insert IDs. Planetscale driver handles this automatically, but mysql2 dosnt and may need all DB inserts to be modified as a transaction with "LAST_INSERT_ID" being returned.
 
 // create the connection
-const connection = connect({
+// TODO: add support for read replicas, likely hostname will need to change based on deploy location
+const connectionRead = connect({
+  host: process.env['DB_HOST'],
+  username: process.env['DB_USERNAME'],
+  password: process.env['DB_PASSWORD']
+});
+const connectionWrite = connect({
   host: process.env['DB_HOST'],
   username: process.env['DB_USERNAME'],
   password: process.env['DB_PASSWORD']
@@ -18,7 +24,7 @@ const connectionOptions = {
 };
 
 export const db = {
-  read: drizzle(connection, connectionOptions),
-  write: drizzle(connection, connectionOptions)
+  read: drizzle(connectionRead, connectionOptions),
+  write: drizzle(connectionWrite, connectionOptions)
 };
 export type DBType = typeof db;
