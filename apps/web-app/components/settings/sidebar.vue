ceb4efd3d9506ae1f2c219f180cb98fadeedf455
McPizza <gh@l1.io>
2023-09-25 14:32:46 +0100

    WEB: Update settings sidebar

--- apps/web-app/components/settings/sidebar.vue
+++ apps/web-app/components/settings/sidebar.vue
@@ -6,18 +6,13 @@
   const route = useRoute();
   const router = useRouter();
 
-  watch(
-    settingsSelectedOrg,
-    (newVal) => {
-      const currentOrgRoute = route.params.orgId as string;
-      if (currentOrgRoute) {
-        const newPath = route.path.replace(currentOrgRoute, newVal);
-        router.push(newPath);
-      }
+  watch(settingsSelectedOrg, (newVal) => {
+    const currentOrgRoute = route.params.orgId as string;
+    if (currentOrgRoute) {
+      const newPath = route.path.replace(currentOrgRoute, newVal);
+      router.push(newPath);
     }
-    // ,
-    // { immediate: true }
-  );
+  });
 
   const {
     data: userOrgs,
@@ -31,25 +26,15 @@
     { server: false }
   );
 
-  watch(
-    userOrgs,
-    (newVal) => {
-      console.log('new userOrgs Val', newVal);
-      if (newVal && newVal.userOrgs.length > 0) {
-        if (!userHasAdminOrgs.value) userHasAdminOrgs.value = true;
-        if (!settingsSelectedOrg.value) {
-          settingsSelectedOrg.value = newVal.userOrgs[0].org.publicId;
-        }
+  watch(userOrgs, (newVal) => {
+    console.log('new userOrgs Val', newVal);
+    if (newVal && newVal.userOrgs.length > 0) {
+      if (!userHasAdminOrgs.value) userHasAdminOrgs.value = true;
+      if (!settingsSelectedOrg.value) {
+        settingsSelectedOrg.value = newVal.userOrgs[0].org.publicId;
       }
     }
-    // ,
-    // { immediate: true }
-  );
-
-  // if (userOrgs.value && userOrgs.value.userOrgs.length > 0) {
-  //   userHasAdminOrgs.value = true;
-  //   activeOrg.value = userOrgs.value.userOrgs[0].org.publicId;
-  // }
+  });
 
   // TODO: fix scroll bar positioning, move to right, approx 20 px (may need to move to a parent div)
 </script>
@@ -77,90 +62,95 @@
           </div>
         </div>
       </div>
-      <div>
-        <span class="font-display text-lg">Org</span>
-      </div>
-      <div
-        v-if="pending"
-        class="flex flex-row w-full p-8 bg-base-3 rounded-xl gap-4 justify-center rounded-tl-2xl">
-        <icon
-          name="svg-spinners:3-dots-fade"
-          size="24" />
-        <span>Loading organizations</span>
-      </div>
-      <div
-        v-if="!userHasAdminOrgs && !pending"
-        class="flex flex-row w-full p-8 bg-base-3 rounded-xl gap-4 justify-center rounded-tl-2xl">
-        <icon
-          name="ph-identification-badge"
-          size="24" />
-        <span>You are not an admin in any organizations</span>
-      </div>
-      <div
-        class="flex flex-col gap-2"
-        v-if="userHasAdminOrgs && !pending">
-        <div class="flex flex-col gap-1">
-          <SettingsOrgSelector
-            v-if="userOrgs"
-            v-for="org in userOrgs.userOrgs"
-            :orgData="org"
-            :key="org.org.publicId"
-            @click="settingsSelectedOrg = org.org.publicId"
-            :isActive="settingsSelectedOrg === org.org.publicId" />
-        </div>
+      <div class="flex flex-col gap-4">
         <div>
-          <span
-            class="text-xs uppercase font-semibold text-base-11 border-b-1 border-base-3 pb-1">
-            Setup
-          </span>
+          <span class="font-display text-lg">Org</span>
         </div>
-        <div class="flex flex-col gap-2 pb-2 pl-2">
-          <nuxt-link :to="`/settings/org/${settingsSelectedOrg}`">
-            <span class="text-sm">Org Profile</span>
-          </nuxt-link>
-          <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/members`">
-            <span class="text-sm">Members</span>
-          </nuxt-link>
-          <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/invites`">
-            <span class="text-sm">Invites</span>
-          </nuxt-link>
-          <nuxt-link :to="`settings/org/${settingsSelectedOrg}/groups`">
-            <span class="text-sm">Groups</span>
-          </nuxt-link>
-          <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/features`">
-            <span class="text-sm">Modules/features</span>
-          </nuxt-link>
-          <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/billing`">
-            <span class="text-sm">Billing</span>
-          </nuxt-link>
+        <div
+          v-if="pending"
+          class="flex flex-row w-full p-8 bg-base-3 rounded-xl gap-4 justify-center rounded-tl-2xl">
+          <icon
+            name="svg-spinners:3-dots-fade"
+            size="24" />
+          <span>Loading organizations</span>
         </div>
-        <div>
-          <span
-            class="text-xs uppercase font-semibold text-base-11 border-b-1 border-base-3 pb-1"
-            >Mail</span
-          >
+        <div
+          v-if="!userHasAdminOrgs && !pending"
+          class="flex flex-row w-full p-8 bg-base-3 rounded-xl gap-4 justify-center rounded-tl-2xl">
+          <icon
+            name="ph-identification-badge"
+            size="24" />
+          <span>You are not an admin in any organizations</span>
         </div>
-        <div class="flex flex-col gap-2 pb-2 pl-2">
-          <div>
+        <div
+          class="flex flex-col gap-8"
+          v-if="userHasAdminOrgs && !pending">
+          <div class="flex flex-col gap-1">
+            <SettingsOrgSelector
+              v-if="userOrgs"
+              v-for="org in userOrgs.userOrgs"
+              :orgData="org"
+              :key="org.org.publicId"
+              @click="settingsSelectedOrg = org.org.publicId"
+              :isActive="settingsSelectedOrg === org.org.publicId" />
+          </div>
+          <div class="flex flex-col gap-2 pb-2 pl-2">
+            <span
+              class="text-xs uppercase font-semibold text-base-11 border-b-1 border-base-3 pb-1">
+              Setup
+            </span>
+            <nuxt-link :to="`/settings/org/${settingsSelectedOrg}`">
+              <span class="text-sm">Org Profile</span>
+            </nuxt-link>
+            <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/features`">
+              <span class="text-sm">Modules/features</span>
+            </nuxt-link>
+            <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/billing`">
+              <span class="text-sm">Billing</span>
+            </nuxt-link>
+          </div>
+
+          <div class="flex flex-col gap-2 pb-2 pl-2">
+            <span
+              class="text-xs uppercase font-semibold text-base-11 border-b-1 border-base-3 pb-1">
+              Users
+            </span>
+            <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/members`">
+              <span class="text-sm">Members</span>
+            </nuxt-link>
+            <nuxt-link :to="`/settings/org/${settingsSelectedOrg}/invites`">
+              <span class="text-sm">Invites</span>
+            </nuxt-link>
+            <nuxt-link :to="`settings/org/${settingsSelectedOrg}/groups`">
+              <span class="text-sm">Groups</span>
+            </nuxt-link>
+          </div>
+
+          <div class="flex flex-col gap-2 pb-2 pl-2">
+            <span
+              class="text-xs uppercase font-semibold text-base-11 border-b-1 border-base-3 pb-1">
+              Mail
+            </span>
             <nuxt-link
               :to="`/settings/org/${settingsSelectedOrg}/mail/domains`">
               <span class="text-sm">Domains</span>
             </nuxt-link>
-          </div>
-          <div>
-            <span class="text-sm">Email identities</span>
-          </div>
-          <div>
-            <span class="text-sm">Routing Rules</span>
-          </div>
-          <div>
-            <span class="text-sm">Forwarding address</span>
-          </div>
-          <div>
-            <span class="text-sm">External identities</span>
-          </div>
-          <div>
-            <span class="text-sm">Limits</span>
+            <nuxt-link
+              :to="`/settings/org/${settingsSelectedOrg}/mail/addresses`">
+              <span class="text-sm">Email Addresses</span>
+            </nuxt-link>
+            <nuxt-link
+              :to="`/settings/org/${settingsSelectedOrg}/mail/addresses`">
+              <span class="text-sm">Routing Rules</span>
+            </nuxt-link>
+            <nuxt-link
+              :to="`/settings/org/${settingsSelectedOrg}/mail/addresses`">
+              <span class="text-sm">External identities</span>
+            </nuxt-link>
+            <nuxt-link
+              :to="`/settings/org/${settingsSelectedOrg}/mail/addresses`">
+              <span class="text-sm">Limits</span>
+            </nuxt-link>
           </div>
         </div>
       </div>
