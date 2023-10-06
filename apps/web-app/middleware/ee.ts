eae17ccf7df1d6dcc044d131f4af556de64db3d8
McPizza <gh@l1.io>
2023-10-06 15:21:45 +0100

    WEB: add EE middleware

---
+++ apps/web-app/middleware/ee.ts
@@ -0,0 +1,8 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+  const eeConfig = useRuntimeConfig().public.ee;
+  if (eeConfig.enabled) {
+    return;
+  }
+
+  return navigateTo('/h');
+});
