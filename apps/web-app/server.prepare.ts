276ec94c24a3a6e84b2289c5c61abb633a231b60
McPizza <gh@l1.io>
2023-10-06 14:41:50 +0100

    WEB: Update server.prepare for EE

--- apps/web-app/server.prepare.ts
+++ apps/web-app/server.prepare.ts
@@ -23,20 +23,33 @@ export default defineNuxtPrepareHandler(async () => {
 
   // Check for EE license, enable billing functionality
 
+  const eeConfig = {
+    enabled: false,
+    modules: [] as string[]
+  };
+
   const billingConfig = {
     enabled: false,
     billingUrl: '',
     billingKey: ''
   };
+
   const eeLicenseKey = process.env.EE_LICENSE_KEY;
+  if (eeLicenseKey) {
+    console.log('✅ Enterprise Edition is enabled');
+    eeConfig.enabled = true;
+  }
+
   const billingUrl = process.env.BILLING_URL;
   const billingKey = process.env.BILLING_KEY;
   if (eeLicenseKey && billingUrl && billingKey) {
-    console.log('🚨 Billing is enabled');
+    console.log('✅ EE Billing module is enabled');
+    eeConfig.modules.push('billing');
     billingConfig.enabled = true;
     billingConfig.billingUrl = billingUrl;
     billingConfig.billingKey = billingKey;
   }
+  const eeEnabled = eeLicenseKey ? true : false;
 
   return {
     // Overwrite the runtime config variable `foo`
@@ -49,7 +62,8 @@ export default defineNuxtPrepareHandler(async () => {
         ) as MailDomainEntries[],
         mailDomainPremium: JSON.parse(
           mailDomainPremiumEnv
-        ) as MailDomainEntries[]
+        ) as MailDomainEntries[],
+        eeConfig
       }
     }
   };
