455f38ecf7a897c504f50f58032ad56a326e9a82
McPizza <gh@l1.io>
2023-11-17 19:43:00 +0000

    WEB: Add util to gen avatar URLs

--- apps/web-app/composables/utils.ts
+++ apps/web-app/composables/utils.ts
@@ -1,7 +1,15 @@
 import { cva, type VariantProps } from 'class-variance-authority';
+function generateAvatarUrl(id: string, size: string | undefined) {
+  const imageUrlAccountHash = useRuntimeConfig().public.cfImagesAccountHash;
+  if (imageUrlAccountHash) {
+    return `https://imagedelivery.net/${imageUrlAccountHash}/${id}/${
+      size ? size : ''
+    }`;
+  }
+}
 
 export const useUtils = () => {
-  return { cva };
+  return { cva, generateAvatarUrl };
 };
 
 // TODO: Fix exporting types under namespace UseUtilTypes
