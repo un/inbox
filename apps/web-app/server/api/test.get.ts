cb6a78414178aec1698fe4f47b69665631895a80
McPizza <gh@l1.io>
2023-12-07 13:26:49 +0000

    REPO: remove test files

--- apps/web-app/server/api/test.get.ts
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(async (event) => {
-  const response = new Date().toString();
-
-  await setTimeout(() => {
-    console.log('done');
-  }, 1000);
-  return response;
-});
