c6497a3cbee198cd07602de6467ce4711eccf9c6
McPizza <gh@l1.io>
2024-01-26 15:37:56 +0000

    dedicated convo avatar

---
+++ apps/web-app/components/convos/convoAvatar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import type { ConvoParticipantEntry } from '~/composables/types';
+  type Props = {
+    participantPublicId: string;
+    participants: ConvoParticipantEntry[];
+    size?: 'xs' | 'sm' | 'md' | 'lg';
+  };
+
+  const props = defineProps<Props>();
+  const participant = computed(() => {
+    return props.participants.find(
+      (participant) =>
+        participant.participantPublicId === props.participantPublicId
+    );
+  });
+</script>
+<template>
+  <UnUiAvatar
+    v-if="participant"
+    :public-id="participant.avatarPublicId"
+    :alt="participant.name"
+    :type="participant.type"
+    :color="participant.color"
+    :show-icon="true"
+    :size="props.size ? props.size : 'sm'" />
+</template>
