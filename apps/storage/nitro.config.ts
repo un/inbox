24727b9b92aa8d25275f4f8b101f528271d1281a
McPizza <gh@l1.io>
2024-02-01 21:07:44 +0000

    update cors

--- apps/storage/nitro.config.ts
+++ apps/storage/nitro.config.ts
@@ -16,6 +16,7 @@ export default defineNitroConfig({
     '/api/**': {
       cors: true,
       headers: {
+        'Access-Control-Allow-Methods': 'GET, POST',
         'Access-Control-Allow-Origin': process.env.WEBAPP_URL,
         'Access-Control-Allow-Credentials': 'true'
       }
