4829afc5ea35aff679f13a8b905ad14ac9dc6bb6
McPizza <gh@l1.io>
2024-02-08 14:38:27 +0000

    fix(web): :ambulance: hotfix nuxt layout shift bug
    temp hotfix for https://github.com/nuxt/nuxt/issues/25214

--- apps/web-app/pages/[orgSlug]/index.vue
+++ apps/web-app/pages/[orgSlug]/index.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
   const orgSlug = useRoute().params.orgSlug as string;
-  navigateTo(`/${orgSlug}/convo`);
+
+  reloadNuxtApp({
+    path: `/${orgSlug}/convo`,
+    force: true,
+    ttl: 5000
+  });
+
+  // navigateTo(`/${orgSlug}/convo`);
 </script>
 <template><div></div></template>
