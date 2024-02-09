d2d85ec87e824d65fde4860d2cf8918bddbbe9fa
McPizza <gh@l1.io>
2024-02-09 16:54:54 +0000

    add passkey db utils

---
+++ apps/web-app/server/utils/auth/passkeyDbAdaptor.ts
@@ -0,0 +1,161 @@
+import { eq } from 'drizzle-orm';
+import { Base64Encoding } from 'oslo/encoding';
+import { db } from '@uninbox/database';
+import { authenticators } from '@uninbox/database/schema';
+
+//! Enable debug logging
+const debug = true;
+const log = (...args: any[]) => {
+  if (debug) {
+    console.log('🔐 Passkey Auth DB Adapter', ...args);
+  }
+};
+
+//* Utils
+const base64 = new Base64Encoding(
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+);
+async function transformDbToAuthAuthenticator(
+  dbQuery: NonNullable<
+    Awaited<ReturnType<typeof db.query.authenticators.findFirst>>
+  >
+): Promise<Authenticator> {
+  return {
+    accountId: dbQuery.accountId,
+    credentialID: base64.decode(dbQuery.credentialID),
+    credentialPublicKey: base64.decode(dbQuery.credentialPublicKey),
+    counter: Number(dbQuery.counter),
+    credentialDeviceType: dbQuery.credentialDeviceType as CredentialDeviceType,
+    credentialBackedUp: dbQuery.credentialBackedUp,
+    transports: dbQuery.transports as AuthenticatorTransport[] | undefined
+  };
+}
+
+//* Passkey DB
+export type CredentialDeviceType = 'singleDevice' | 'multiDevice';
+export interface Authenticator {
+  accountId: number;
+  credentialID: Uint8Array;
+  credentialPublicKey: Uint8Array;
+  counter: number;
+  credentialDeviceType: CredentialDeviceType;
+  credentialBackedUp: boolean;
+  transports?: AuthenticatorTransport[];
+}
+
+async function createAuthenticator(authenticator: Authenticator) {
+  log('passkey: createAuthenticator', { authenticator });
+  const b64ID = base64.encode(authenticator.credentialID);
+  const b64PK = base64.encode(authenticator.credentialPublicKey);
+
+  await db.insert(authenticators).values({
+    accountId: authenticator.accountId,
+    credentialID: b64ID,
+    credentialPublicKey: b64PK,
+    counter: BigInt(authenticator.counter),
+    credentialDeviceType: authenticator.credentialDeviceType,
+    credentialBackedUp: authenticator.credentialBackedUp,
+    transports: authenticator.transports
+  });
+
+  return authenticator;
+}
+
+async function updateAuthenticatorCounter(
+  authenticator: Pick<Authenticator, 'credentialID'>,
+  newCounter: number
+) {
+  log('passkey: updateAuthenticatorCounter', {
+    authenticator,
+    newCounter
+  });
+  const b64ID = base64.encode(authenticator.credentialID);
+
+  const authenticatorObject = await db.query.authenticators.findFirst({
+    where: eq(authenticators.credentialID, b64ID),
+    columns: {
+      counter: true
+    }
+  });
+  if (!authenticatorObject) throw new Error('Authenticator not found');
+
+  await db
+    .update(authenticators)
+    .set({
+      counter: BigInt(newCounter)
+    })
+    .where(eq(authenticators.credentialID, b64ID));
+
+  const updatedAuthenticator = {
+    ...authenticator,
+    counter: +newCounter
+  } as Authenticator;
+  return updatedAuthenticator;
+}
+
+async function getAuthenticator(credentialId: Uint8Array) {
+  log('passkey: getAuthenticator', { credentialId });
+  const b64ID = asBase64(credentialId);
+
+  const dbQuery = await db.query.authenticators.findFirst({
+    where: eq(authenticators.credentialID, b64ID),
+    columns: {
+      id: true,
+      accountId: true,
+      credentialID: true,
+      credentialPublicKey: true,
+      counter: true,
+      credentialDeviceType: true,
+      credentialBackedUp: true,
+      transports: true
+    }
+  });
+  if (!dbQuery) return null;
+  const decodedResult: Authenticator =
+    await transformDbToAuthAuthenticator(dbQuery);
+
+  return decodedResult;
+}
+
+async function deleteAuthenticator(credentialId: Uint8Array) {
+  log('passkey: deleteAuthenticator', { credentialId });
+  const b64ID = base64.encode(credentialId);
+
+  const dbDeleteResult = await db
+    .delete(authenticators)
+    .where(eq(authenticators.credentialID, b64ID));
+
+  return dbDeleteResult.rowsAffected > 0;
+}
+
+async function listAuthenticatorsByAccountId(accountId: number) {
+  log('passkey: listAuthenticatorsByAccountId', { accountId });
+  const dbQuery = await db.query.authenticators.findMany({
+    where: eq(authenticators.accountId, accountId),
+    columns: {
+      id: true,
+      accountId: true,
+      credentialID: true,
+      credentialPublicKey: true,
+      counter: true,
+      credentialDeviceType: true,
+      credentialBackedUp: true,
+      transports: true
+    }
+  });
+  const decodedResults: Authenticator[] = await Promise.all(
+    dbQuery.map(
+      async (authenticator) =>
+        await transformDbToAuthAuthenticator(authenticator)
+    )
+  );
+  return decodedResults;
+}
+
+export const usePasskeysDb = {
+  createAuthenticator: createAuthenticator,
+  updateAuthenticatorCounter: updateAuthenticatorCounter,
+  getAuthenticator: getAuthenticator,
+  deleteAuthenticator: deleteAuthenticator,
+  listAuthenticatorsByAccountId: listAuthenticatorsByAccountId
+};
