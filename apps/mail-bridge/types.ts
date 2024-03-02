03abc4f0d7374e23e588dec2d389a425e863b9bc
McPizza <gh@l1.io>
2024-03-02 00:31:39 +0000

    mid debug

--- apps/mail-bridge/types.ts
+++ apps/mail-bridge/types.ts
@@ -6,36 +6,7 @@ export interface postalEmailPayload {
   id: number;
   rcpt_to: string;
   mail_from: string;
-  token: string;
-  subject: string;
-  message_id: string;
-  timestamp: number;
-  size: string;
-  spam_status: 'Spam' | 'NotSpam';
-  bounce: boolean;
-  received_with_ssl: boolean | null;
-  to: string;
-  cc: string | null;
-  from: string;
-  date: string;
-  in_reply_to: string | null;
-  references: string | null;
-  plain_body: string;
-  html_body: string;
-  auto_submitted:
-    | 'no'
-    | 'auto-generated'
-    | 'auto-replied'
-    | 'auto-notified'
-    | null;
-  attachment_quantity: number;
-  attachments: PostalEmailAttachment[] | [];
-  replies_from_plain_body?: string | null;
-}
-
-export interface PostalEmailAttachment {
-  filename: string;
-  content_type: string;
+  message: string;
+  base64: boolean;
   size: number;
-  data: string;
 }
