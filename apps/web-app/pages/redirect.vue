e30937b299becf94d96da30f5daeb0029a68a053
McPizza <gh@l1.io>
2024-03-05 13:11:48 +0000

    added comments to clarify redirects

--- apps/web-app/pages/redirect.vue
+++ apps/web-app/pages/redirect.vue
@@ -9,6 +9,8 @@
     if (!defaultOrgSlug) {
       return navigateTo(`/join/org`);
     }
+    // We need to redirect to the index page of [orgSlug] due to this nuxt issue https://github.com/nuxt/nuxt/issues/25214
+    // the index page will reload nuxt, then redirect to the convos view - we should not directly navigate to convos page!!!
     setTimeout(() => {
       navigateTo(`/${defaultOrgSlug}`);
     }, 500);
