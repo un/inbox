8cc21659fdd7c206f18faae5f60a4af4a8e8eb4c
Rahul Mishra <blankparticle@gmail.com>
2024-03-08 15:49:44 +0530

    chore(repo): split tsconfig and mailtools to seperate repo (#126)

--- packages/mailtools/src/index.ts
+++
@@ -1,8 +0,0 @@
-import linkify from './linkify';
-import { parseMessage } from './parseMessage';
-import { blockRemoteContent } from './blockRemoteContent';
-import type { ReplacementOptions } from './blockRemoteContent';
-import type { ParseMessageOptions } from './parseMessage';
-
-export { parseMessage, linkify, blockRemoteContent };
-export type { ParseMessageOptions, ReplacementOptions };
--- packages/mailtools/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ['src/index.ts'],
-  dts: true,
-  splitting: false,
-  treeshake: true,
-  format: 'esm'
-});
