1fd14fb683842d0edf75374e9503c88ebff8acd5
McPizza <gh@l1.io>
2024-03-14 16:42:35 +0000

    fix verifyPostalWebhooks

--- apps/mail-bridge/middleware/verifyPostalSig.ts
+++ apps/mail-bridge/middleware/verifyPostalSig.ts
@@ -1,15 +1,18 @@
 import { validatePostalWebhookSignature } from '../utils/validatePostalWebhookSignature';
-
+import type { PostalConfig } from '../types';
 export default defineEventHandler(async (event) => {
   if (getRequestURL(event).pathname.startsWith('/postal')) {
     const body = await readBody(event);
     const signature = getHeader(event, 'x-postal-signature');
     //TODO: need to support multiple public keys from multiple servers, and return true if any match
-    const publicKey = useRuntimeConfig().postalWebhookPublicKey;
+    const postalConfig: PostalConfig = useRuntimeConfig().postal;
+    const publicKeys = postalConfig.servers.map(
+      (server) => server.webhookPubKey
+    );
     const validPostalSignature = await validatePostalWebhookSignature(
       body,
       signature,
-      publicKey
+      publicKeys
     );
     if (!validPostalSignature) {
       sendNoContent(event, 401);
