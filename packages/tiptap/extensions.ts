070442741091a4ba7001547585a9e18bfb368a4d
McPizza <gh@l1.io>
2024-03-14 13:05:11 +0000

    fix tiptap extensions

--- packages/tiptap/extensions.ts
+++ packages/tiptap/extensions.ts
@@ -2,7 +2,6 @@ import StarterKit from '@tiptap/starter-kit';
 import ExtensionTextSyle from '@tiptap/extension-text-style';
 import ExtensionColor from '@tiptap/extension-color';
 import type { AnyExtension } from '@tiptap/vue-3';
-export const tipTapExtensions: AnyExtension[] = [StarterKit];
 export const tipTapExtensions: AnyExtension[] = [
   StarterKit,
   ExtensionTextSyle,
