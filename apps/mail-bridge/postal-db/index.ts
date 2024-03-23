fdb4ddbc1b10791953dffec23e3121b0d3b86d72
McPizza <gh@l1.io>
2024-03-23 14:24:41 +0000

    add no local postal db workaround

--- apps/mail-bridge/postal-db/index.ts
+++ apps/mail-bridge/postal-db/index.ts
@@ -2,9 +2,15 @@ import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
 import { useRuntimeConfig } from '#imports';
+import type { PostalConfig } from '../types';
 
+const postalConfig: PostalConfig = useRuntimeConfig().postal;
+
+const isLocal = postalConfig.localMode;
 export const connection = await mysql.createConnection({
-  uri: `${(useRuntimeConfig().postal as any).activeServers.dbConnectionString}/postal`,
+  uri: isLocal
+    ? process.env.DB_MYSQL_MIGRATION_URL
+    : `${(useRuntimeConfig().postal as any).activeServers.dbConnectionString}/postal`,
   multipleStatements: true
 });
 
