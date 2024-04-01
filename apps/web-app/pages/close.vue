61fd965450ce6d19d9057c1ee9ef30b6d750a091
McPizza <gh@l1.io>
2024-04-01 01:04:55 +0100

    fix(web): handle subscription refresh

---
+++ apps/web-app/pages/close.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex h-full w-full items-center justify-center">
+    <span class="font-display text-3xl">You can now close this page</span>
+  </div>
+</template>
