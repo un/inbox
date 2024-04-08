1ec08d59a014e40f04d0a6b380296fe3118fffc9
McPizza <gh@l1.io>
2024-04-08 12:51:08 +0100

    fix(web): personal forwarding address ui

--- apps/platform/trpc/routers/userRouter/addressRouter.ts
+++ apps/platform/trpc/routers/userRouter/addressRouter.ts
@@ -49,7 +49,8 @@ export const addressRouter = router({
                 publicId: true,
                 sendName: true,
                 username: true,
-                domainName: true
+                domainName: true,
+                forwardingAddress: true
               }
             }
           }
