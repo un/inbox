9583d11543c8284c10c08dfee9325e2d95b2b56f
McPizza <gh@l1.io>
2024-04-16 19:20:14 +0100

    fix(web): mobile show hidden convo view

--- apps/web-app/pages/[orgShortcode]/convo/index.vue
+++ apps/web-app/pages/[orgShortcode]/convo/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
-  import { navigateTo, useRoute } from '#imports';
+  import { navigateTo, useRoute, ref } from '#imports';
   import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
 
   const breakpoints = useBreakpoints(breakpointsTailwind);
   const isMobile = breakpoints.smaller('lg'); // only smaller than lg
 
   const orgShortcode = useRoute().params.orgShortcode as string;
+
+  const view = ref<'convo' | 'hidden'>('convo');
+  function toggleView() {
+    view.value = view.value === 'convo' ? 'hidden' : 'convo';
+  }
 </script>
 <template>
   <div
@@ -25,17 +30,32 @@
     <div
       v-if="isMobile"
       class="h-full w-full">
-      <div class="flex h-full max-h-full flex-col-reverse gap-2 lg:flex-col">
+      <div class="flex h-full max-h-full flex-col-reverse gap-2 md:flex-col">
         <div class="flex grow flex-col gap-0 overflow-hidden">
-          <convos-convo-list />
+          <convos-convo-list v-if="view === 'convo'" />
+          <convos-hidden-convo-list v-if="view === 'hidden'" />
         </div>
-
-        <UnUiButton
-          label="New"
-          block
-          icon="i-ph-plus"
-          variant="outline"
-          :to="`/${orgShortcode}/convo/new`" />
+        <span
+          v-if="view === 'hidden'"
+          class="text-base-11 w-full text-center text-xs">
+          SHOWING HIDDEN CONVERSATIONS
+        </span>
+        <div class="flex flex-row gap-2">
+          <UnUiButton
+            class="flex-grow justify-center"
+            label="New"
+            icon="i-ph-plus"
+            variant="outline"
+            :to="`/${orgShortcode}/convo/new`" />
+          <div>
+            <UnUiButton
+              class="flex-grow justify-center"
+              square
+              :icon="view === 'convo' ? 'i-ph-eye' : 'i-ph-eye-slash'"
+              variant="outline"
+              @click="toggleView()" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
