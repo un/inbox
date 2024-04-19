b99cca1072c98e29985ae687abcfc5523ed8284e
Rahul Mishra <blankparticle@gmail.com>
2024-04-19 16:34:41 +0530

    chore: remove unused upstash driver code (#341)
    ## What does this PR do?
    
    Removes unused upstash code and env variables from example env
    
    <!-- If there isn't an issue for this PR, please re-review our Contributing Guide and create an issue -->
    
    ## Type of change
    
    <!-- Please mark the relevant points by using [x] -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] Chore (refactoring code, technical debt, workflow improvements)
    - [ ] Enhancement (small improvements)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## Checklist
    
    <!-- We're starting to get more and more contributions. Please help us making this efficient for all of us and go through this checklist. Please tick off what you did  -->
    
    ### Required
    
    - [ ] Read [Contributing Guide](https://github.com/un/inbox/blob/main/CONTRIBUTING.md)
    - [ ] Self-reviewed my own code
    - [ ] Tested my code in a local environment
    - [ ] Commented on my code in hard-to-understand areas
    - [ ] Checked for warnings, there are none
    - [ ] Removed all `console.logs`
    - [ ] Merged the latest changes from main onto my branch with `git pull origin main`
    - [ ] My changes don't cause any responsiveness issues
    
    ### Appreciated
    
    - [ ] If a UI change was made: Added a screen recording or screenshots to this PR
    - [ ] Updated the UnInbox Docs if changes were necessary

--- apps/storage/plugins/storage.ts
+++ apps/storage/plugins/storage.ts
@@ -1,39 +1,18 @@
 import redisDriver from 'unstorage/drivers/redis';
-import upstashDriver from './upstashDriver';
 import { defineNitroPlugin, useStorage } from '#imports';
 
 export default defineNitroPlugin(() => {
-  let useUpstash: boolean;
-  if (!process.env.DB_UPSTASH_URL || !process.env.DB_UPSTASH_TOKEN) {
-    useUpstash = false;
-  } else {
-    useUpstash = true;
-  }
-
   const storage = useStorage();
 
-  function getCacheDb(base: string) {
-    if (useUpstash) {
-      return upstashDriver({
-        envPrefix: 'DB_UPSTASH',
-        ttl:
-          process.env.NODE_ENV === 'development'
-            ? 60 * 60 * 12
-            : 60 * 60 * 24 * 30,
-        base: base
-      });
-    } else {
-      return redisDriver({
-        url: process.env.DB_REDIS_CONNECTION_STRING,
-        ttl:
-          process.env.NODE_ENV === 'development'
-            ? 60 * 60 * 12
-            : 60 * 60 * 24 * 30,
-        base: base
-      });
-    }
-  }
+  const getCacheDb = (base: string) =>
+    redisDriver({
+      url: process.env.DB_REDIS_CONNECTION_STRING,
+      ttl:
+        process.env.NODE_ENV === 'development'
+          ? 60 * 60 * 12
+          : 60 * 60 * 24 * 30,
+      base
+    });
 
-  // Mount driver
   storage.mount('sessions', getCacheDb('sessions'));
 });
