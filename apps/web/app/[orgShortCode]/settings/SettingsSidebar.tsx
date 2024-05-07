b490229a7d792c7dd9337060eae4bef679121b3f
Ankur Sharma <97390674+Ankur1493@users.noreply.github.com>
2024-05-07 16:41:04 +0530

    created settings sidebar (#419)
    ## What does this PR do?
    
    Created the sidebar for settings page, completed the functionality, some pages are not yet created so will give 404, otherwise it's working here are some ss
    ![Screenshot from 2024-05-04 18-31-10](https://github.com/un/inbox/assets/97390674/560eb56e-79fd-4b80-85ea-dd7dd4613a56)
    ![Screenshot from 2024-05-04 18-31-43](https://github.com/un/inbox/assets/97390674/272af709-1d63-4995-bc36-74892073d59d)
    
    
    Fixes #417 
    
    <!-- If there isn't an issue for this PR, please re-review our Contributing Guide and create an issue -->
    
    ## Type of change
    
    <!-- Please mark the relevant points by using [x] -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] Chore (refactoring code, technical debt, workflow improvements)
    - [ ] Enhancement (small improvements)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    ## Checklist
    
    <!-- We're starting to get more and more contributions. Please help us making this efficient for all of us and go through this checklist. Please tick off what you did  -->
    
    ### Required
    
    - [x] Read [Contributing Guide](https://github.com/un/inbox/blob/main/CONTRIBUTING.md)
    - [x] Self-reviewed my own code
    - [x] Tested my code in a local environment
    - [x] Commented on my code in hard-to-understand areas
    - [x] Checked for warnings, there are none
    - [x] Removed all `console.logs`
    - [x] Merged the latest changes from main onto my branch with `git pull origin main`
    - [x] My changes don't cause any responsiveness issues
    
    ### Appreciated
    
    - [x] If a UI change was made: Added a screen recording or screenshots to this PR
    - [x] Updated the UnInbox Docs if changes were necessary

--- apps/web/app/[orgShortCode]/settings/SettingsSidebar.tsx
+++ apps/web/app/[orgShortCode]/settings/SettingsSidebar.tsx
@@ -1,11 +1,218 @@
 'use client';
 
-import { Flex } from '@radix-ui/themes';
+import { type ReactNode } from 'react';
+import { usePathname } from 'next/navigation';
+import { Flex, Text, Container } from '@radix-ui/themes';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { api } from '@/lib/trpc';
+import { useGlobalStore } from '@/providers/global-store-provider';
+import {
+  CircleUser,
+  Mail,
+  Lock,
+  Building2,
+  CreditCard,
+  AtSign,
+  Globe,
+  Users,
+  UserRoundPlus,
+  HeartHandshake
+} from 'lucide-react';
+
+type NavLinks = {
+  label: string;
+  to: string;
+  icon: ReactNode;
+};
 
 export default function SettingsSidebar() {
+  const orgShortCode = useGlobalStore((state) => state.currentOrg.shortCode);
+  const pathname = usePathname();
+
+  const { data: isAdmin } = api.org.users.members.isOrgMemberAdmin.useQuery({
+    orgShortCode
+  });
+
+  const personalLinks: NavLinks[] = [
+    {
+      label: 'Profile',
+      to: `/${orgShortCode}/settings/user/profile`,
+      icon: <CircleUser />
+    },
+    {
+      label: 'Personal Addresses',
+      to: `/${orgShortCode}/settings/user/addresses`,
+      icon: <Mail />
+    },
+    {
+      label: 'Security',
+      to: `/${orgShortCode}/settings/user/security`,
+      icon: <Lock />
+    }
+  ];
+  const orgSetupLinks: NavLinks[] = [
+    {
+      label: 'Org Profile',
+      to: `/${orgShortCode}/settings/org`,
+      icon: <Building2 />
+    },
+    {
+      label: 'Billing',
+      to: `/${orgShortCode}/settings/org/setup/billing`,
+      icon: <CreditCard />
+    }
+  ];
+
+  const orgUserLinks: NavLinks[] = [
+    {
+      label: 'Members',
+      to: `/${orgShortCode}/settings/org/users/members`,
+      icon: <Users />
+    },
+    {
+      label: 'Invites',
+      to: `/${orgShortCode}/settings/org/users/invites`,
+      icon: <UserRoundPlus />
+    },
+    {
+      label: 'Teams',
+      to: `/${orgShortCode}/settings/org/users/teams`,
+      icon: <HeartHandshake />
+    }
+  ];
+  const orgMailLinks: NavLinks[] = [
+    {
+      label: 'Domains',
+      to: `/${orgShortCode}/settings/org/mail/domains`,
+      icon: <Globe />
+    },
+    {
+      label: 'Email Addresses',
+      to: `/${orgShortCode}/settings/org/mail/addresses`,
+      icon: <AtSign />
+    }
+  ];
+
   return (
-    <Flex className="bg-slate-2 dark:bg-slatedark-2 h-full w-[350px] p-2">
-      Settings Side Bar
+    <Flex
+      gap="4"
+      className="bg-slate-2 dark:bg-slatedark-2  h-full w-[400px] flex-col  p-2 px-4">
+      <Text
+        size="5"
+        weight="bold"
+        color="gray">
+        Personal
+      </Text>
+      <Flex
+        className="flex-col"
+        gap="1">
+        {personalLinks.map(({ label, to, icon }) => (
+          <Link
+            key={to}
+            href={to}>
+            <Flex
+              gap="4"
+              className={cn(
+                pathname === to ? 'dark:bg-gray-10 bg-gray-4' : '',
+                'rounded p-1 pl-2'
+              )}>
+              {icon}
+              <div>{label}</div>
+            </Flex>
+          </Link>
+        ))}
+      </Flex>
+      {isAdmin && (
+        <Container className="border-t pt-1">
+          <Text
+            size="5"
+            weight="bold"
+            color="gray">
+            Organization
+          </Text>
+
+          <Flex
+            className="flex-col pt-4"
+            gap="1">
+            <Text
+              className="pb-1 pl-1"
+              size="3"
+              weight="bold"
+              color="cyan">
+              Setup
+            </Text>
+            {orgSetupLinks.map(({ label, to, icon }) => (
+              <Link
+                key={to}
+                href={to}>
+                <Flex
+                  gap="4"
+                  className={cn(
+                    pathname === to ? 'dark:bg-gray-10 bg-gray-4' : '',
+                    'rounded p-1 pl-2'
+                  )}>
+                  {icon}
+                  <div>{label}</div>
+                </Flex>
+              </Link>
+            ))}
+          </Flex>
+          <Flex
+            className="flex-col"
+            gap="1">
+            <Text
+              className="pb-1 pl-1"
+              size="3"
+              weight="bold"
+              color="cyan">
+              Users
+            </Text>
+            {orgUserLinks.map(({ label, to, icon }) => (
+              <Link
+                key={to}
+                href={to}>
+                <Flex
+                  gap="4"
+                  className={cn(
+                    pathname === to ? 'dark:bg-gray-10 bg-gray-4' : '',
+                    'rounded p-1 pl-2'
+                  )}>
+                  {icon}
+                  <div>{label}</div>
+                </Flex>
+              </Link>
+            ))}
+          </Flex>
+
+          <Flex
+            className="flex-col"
+            gap="1">
+            <Text
+              className="pb-1 pl-1"
+              size="3"
+              weight="bold"
+              color="cyan">
+              Mail
+            </Text>
+            {orgMailLinks.map(({ label, to, icon }) => (
+              <Link
+                key={to}
+                href={to}>
+                <Flex
+                  gap="4"
+                  className={cn(
+                    pathname === to ? 'dark:bg-gray-10 bg-gray-4' : '',
+                    'rounded p-1 pl-2'
+                  )}>
+                  {icon}
+                  <div>{label}</div>
+                </Flex>
+              </Link>
+            ))}
+          </Flex>
+        </Container>
+      )}
     </Flex>
   );
 }
