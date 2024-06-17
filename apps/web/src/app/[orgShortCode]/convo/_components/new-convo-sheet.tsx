78f8bb219aa01eab016c34129d49923c2c4c1077
Aashish Upadhyay <boysmall300@gmail.com>
2024-06-18 00:14:10 +0545

    changed to shadcn drawer

--- apps/web/src/app/[orgShortCode]/convo/_components/new-convo-sheet.tsx
+++ apps/web/src/app/[orgShortCode]/convo/_components/new-convo-sheet.tsx
@@ -3,37 +3,41 @@
 import { Button } from '@/src/components/shadcn-ui/button';
 import { Plus, X } from '@phosphor-icons/react';
 import { useState } from 'react';
-import { Drawer } from 'vaul';
+
 import CreateConvoForm from './create-convo-form';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerPortal,
+  DrawerTrigger
+} from '@/src/components/shadcn-ui/drawer';
 
 export function NewConvoSheet() {
   const [open, setOpen] = useState(false);
 
   return (
-    <Drawer.Root
+    <Drawer
       dismissible={false}
       open={open}
       direction="bottom"
       modal={false}>
-      <Drawer.Trigger
+      <DrawerTrigger
         asChild
         onClick={() => setOpen(true)}>
         <Button className="fixed bottom-4 right-4">
           <Plus className="mr-1 h-4 w-4" /> Convo
         </Button>
-      </Drawer.Trigger>
-      <Drawer.Portal>
-        <Drawer.Content className="bg-popover fixed bottom-0 right-0 flex max-h-[36rem] w-full flex-col overflow-hidden rounded-lg border md:right-4 md:w-[30rem]">
-          <Button
-            variant={'ghost'}
-            onClick={() => setOpen(false)}
-            className="fixed right-1 top-1">
-            <X className="h-4 w-4" />
-          </Button>
+      </DrawerTrigger>
+      <DrawerContent className="bg-popover fixed bottom-0 right-0 flex max-h-[36rem] w-full flex-col overflow-hidden rounded-lg border md:right-4 md:w-[30rem]">
+        <Button
+          variant={'ghost'}
+          onClick={() => setOpen(false)}
+          className="fixed right-1 top-1">
+          <X className="h-4 w-4" />
+        </Button>
 
-          <CreateConvoForm />
-        </Drawer.Content>
-      </Drawer.Portal>
-    </Drawer.Root>
+        <CreateConvoForm />
+      </DrawerContent>
+    </Drawer>
   );
 }
