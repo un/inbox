9689eac4896a20a5ebb1e484d1f5e72473169630
Rahul Mishra <blankparticle@gmail.com>
2024-07-02 22:12:27 +0530

    fix: replace convo list with Virtuoso component to fix bugs (#536)

--- apps/web/src/app/[orgShortCode]/convo/_components/convo-list.tsx
+++ apps/web/src/app/[orgShortCode]/convo/_components/convo-list.tsx
@@ -2,8 +2,8 @@
 
 import { api } from '@/src/lib/trpc';
 import { useGlobalStore } from '@/src/providers/global-store-provider';
-import { useEffect, useRef } from 'react';
-import { useVirtualizer } from '@tanstack/react-virtual';
+import { useCallback } from 'react';
+import { Virtuoso } from 'react-virtuoso';
 import { ms } from '@u22n/utils/ms';
 import { ConvoItem } from './convo-list-item';
 
@@ -13,7 +13,7 @@ type Props = {
 
 export function ConvoList(props: Props) {
   const orgShortCode = useGlobalStore((state) => state.currentOrg.shortCode);
-  const scrollableRef = useRef(null);
+
   const {
     data: convos,
     fetchNextPage,
@@ -32,73 +32,36 @@ export function ConvoList(props: Props) {
   );
 
   const allConvos = convos ? convos.pages.flatMap(({ data }) => data) : [];
-  const convosVirtualizer = useVirtualizer({
-    count: allConvos.length + (hasNextPage ? 1 : 0),
-    estimateSize: () => 115,
-    getScrollElement: () => scrollableRef.current,
-    overscan: 3
-  });
 
-  useEffect(() => {
-    const lastItem = convosVirtualizer.getVirtualItems().at(-1);
-    if (!lastItem) return;
-    if (
-      lastItem.index >= allConvos.length - 1 &&
-      hasNextPage &&
-      !isFetchingNextPage
-    ) {
-      void fetchNextPage();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    fetchNextPage,
-    hasNextPage,
-    allConvos.length,
-    isFetchingNextPage,
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    convosVirtualizer.getVirtualItems()
-  ]);
+  const itemRenderer = useCallback(
+    (index: number, convo: (typeof allConvos)[number]) => {
+      return (
+        <div key={convo.publicId}>
+          <div className="h-full w-full">
+            <ConvoItem convo={convo} />
+          </div>
+          {index === allConvos.length - 1 && hasNextPage && (
+            <div className="w-full text-center font-bold">Loading...</div>
+          )}
+        </div>
+      );
+    },
+    [allConvos.length, hasNextPage]
+  );
 
   return (
-    <div className="flex h-full w-full flex-col overflow-visible">
+    <div className="flex h-full w-full flex-col">
       {isLoading ? (
-        <div className="w-full text-center font-bold">Loading...</div>
+        <div className="w-full py-2 text-center font-bold">Loading...</div>
       ) : (
-        <>
-          <div
-            className="h-full max-h-full w-full max-w-full scale-100 overflow-y-auto"
-            ref={scrollableRef}>
-            <div
-              className="relative flex w-full max-w-full flex-col"
-              style={{ height: `${convosVirtualizer.getTotalSize()}px` }}>
-              {convosVirtualizer.getVirtualItems().map((virtualItem) => {
-                const isLoader = virtualItem.index > allConvos.length - 1;
-                const convo = allConvos[virtualItem.index]!;
-
-                return (
-                  <div
-                    key={convo.publicId}
-                    data-index={virtualItem.index}
-                    className="absolute left-0 top-0 w-full"
-                    ref={convosVirtualizer.measureElement}
-                    style={{
-                      transform: `translateY(${virtualItem.start}px)`
-                    }}>
-                    {isLoader ? (
-                      <div className="w-full text-center font-bold">
-                        {hasNextPage ? 'Loading...' : ''}
-                      </div>
-                    ) : (
-                      <div className="h-full w-full">
-                        <ConvoItem convo={convo} />
-                      </div>
-                    )}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </>
+        <Virtuoso
+          data={allConvos}
+          itemContent={itemRenderer}
+          style={{ overscrollBehavior: 'contain' }}
+          endReached={async () => {
+            if (hasNextPage && !isFetchingNextPage) await fetchNextPage();
+          }}
+        />
       )}
     </div>
   );
