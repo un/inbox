62330602ef30adb64a5580628c91947c70b94ded
Rahul Mishra <blankparticle@gmail.com>
2024-07-22 21:43:52 +0530

    fix: web middleware fetch TLS issues (#588)

--- apps/web/src/lib/middleware-utils.ts
+++ apps/web/src/lib/middleware-utils.ts
@@ -1,16 +1,27 @@
 import 'server-only';
 import { cookies, headers as nextHeaders } from 'next/headers';
 
-function getBaseUrl() {
+function getPlatformUrl() {
   if (process.env.NEXT_PUBLIC_PLATFORM_URL)
     return process.env.NEXT_PUBLIC_PLATFORM_URL;
   throw new Error('NEXT_PUBLIC_PLATFORM_URL is not set');
 }
 
+function clientHeaders() {
+  const allHeaders = nextHeaders();
+  const clientHeaders = new Headers();
+  const notAllowedHeaders = ['host', 'origin', 'referer'];
+  allHeaders.forEach((value, key) => {
+    if (notAllowedHeaders.includes(key)) return;
+    clientHeaders.append(key, value);
+  });
+  return clientHeaders;
+}
+
 export async function getAuthRedirection() {
   if (!cookies().has('unsession')) return { defaultOrgShortcode: null };
-  return fetch(`${getBaseUrl()}/auth/redirection`, {
-    headers: nextHeaders()
+  return fetch(`${getPlatformUrl()}/auth/redirection`, {
+    headers: clientHeaders()
   }).then((r) => (r.ok ? r.json() : { defaultOrgShortcode: null })) as Promise<{
     defaultOrgShortcode: string | null;
   }>;
@@ -22,8 +33,8 @@ export async function isAuthenticated(shallow = false) {
   if (!cookies().has('unsession')) return false;
   if (shallow) return true;
   try {
-    const data = (await fetch(`${getBaseUrl()}/auth/status`, {
-      headers: nextHeaders()
+    const data = (await fetch(`${getPlatformUrl()}/auth/status`, {
+      headers: clientHeaders()
     }).then((r) => r.json())) as {
       authStatus: 'authenticated' | 'unauthenticated';
     };
