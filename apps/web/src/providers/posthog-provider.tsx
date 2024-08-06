6d63c9b14911bbd9a0e5d3496296cfe68eb72438
Benjamin Shafii <ben@prologe.io>
2024-08-06 18:14:06 +0200

    Improve readability of posthogInit

--- apps/web/src/providers/posthog-provider.tsx
+++ apps/web/src/providers/posthog-provider.tsx
@@ -5,20 +5,27 @@ import { PostHogProvider } from 'posthog-js/react';
 import posthog from 'posthog-js';
 import { env } from '../env';
 
-if (
-  typeof window !== 'undefined' &&
-  env.NEXT_PUBLIC_POSTHOG_ENABLED === 'true'
-) {
+const initPostHog = () => {
   if (!env.NEXT_PUBLIC_POSTHOG_KEY) {
     throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not set');
   }
+
+  const persistence =
+    cookieConsentGiven() === 'yes' ? 'localStorage+cookie' : 'memory';
+
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: '/ingest',
     person_profiles: 'identified_only',
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
-    persistence:
-      cookieConsentGiven() === 'yes' ? 'localStorage+cookie' : 'memory'
+    persistence
   });
+};
+
+if (
+  typeof window !== 'undefined' &&
+  env.NEXT_PUBLIC_POSTHOG_ENABLED === 'true'
+) {
+  initPostHog();
 }
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
