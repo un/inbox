4d325b0d900bcdd38329e27f22f23ff48e8cf2a9
BlankParticle <blankparticle@gmail.com>
2024-08-10 00:46:23 +0530

    chore: make every settings page consistently full width

--- apps/web/src/app/[orgShortcode]/settings/org/users/invites/page.tsx
+++ apps/web/src/app/[orgShortcode]/settings/org/users/invites/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
     });
 
   return (
-    <div className="flex flex-col gap-2 p-4">
+    <div className="flex w-full flex-col gap-2 p-4">
       <PageTitle
         title="Invites"
         description="Grow your Org with Invitations">
