a3878ee60c8fd813e146c87ebb4b1c8f8d2d0442
Rahul Mishra <blankparticle@gmail.com>
2024-08-10 10:23:27 +0530

    feat: allow disabling domains (#672)

--- apps/web/src/app/[orgShortcode]/settings/org/mail/domains/_components/add-domain-modal.tsx
+++ apps/web/src/app/[orgShortcode]/settings/org/mail/domains/_components/add-domain-modal.tsx
@@ -98,7 +98,7 @@ export function AddDomainModal() {
                     domainName: domain
                   });
                 }}
-                disabled={isAddingDomain}>
+                loading={isAddingDomain}>
                 Add Domain
               </Button>
             </div>
