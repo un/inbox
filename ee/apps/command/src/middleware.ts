2f5e4a7c8ce7e0a76f48fbed5560f34b04e2a457
Rahul Mishra <blankparticle@gmail.com>
2024-08-11 12:41:27 +0530

    fix: command to app redirect (#675)

--- ee/apps/command/src/middleware.ts
+++ ee/apps/command/src/middleware.ts
@@ -4,7 +4,7 @@ import { getAccount } from './lib/get-account';
 export default async function middleware(req: NextRequest) {
   const account = await getAccount(req);
   if (!account) {
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_WEBAPP_URL}`);
+    return NextResponse.redirect(`${process.env.WEBAPP_URL}`);
   }
   return NextResponse.next();
 }
