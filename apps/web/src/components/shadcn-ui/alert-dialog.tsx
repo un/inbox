bedd41a4a93501d88962166416dcba1e7561e4cb
BlankParticle <blankparticle@gmail.com>
2024-08-16 11:56:32 +0530

    fix: hide bottom nav in convo and fix dialog text alignment

--- apps/web/src/components/shadcn-ui/alert-dialog.tsx
+++ apps/web/src/components/shadcn-ui/alert-dialog.tsx
@@ -48,10 +48,7 @@ const AlertDialogHeader = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      'flex flex-col space-y-2 text-center sm:text-left',
-      className
-    )}
+    className={cn('flex flex-col space-y-2 text-left', className)}
     {...props}
   />
 );
--- apps/web/src/components/shadcn-ui/dialog.tsx
+++ apps/web/src/components/shadcn-ui/dialog.tsx
@@ -55,10 +55,7 @@ const DialogHeader = ({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
-    className={cn(
-      'flex flex-col space-y-1.5 text-center sm:text-left',
-      className
-    )}
+    className={cn('flex flex-col space-y-1.5 text-left', className)}
     {...props}
   />
 );
