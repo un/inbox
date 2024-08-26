1398977aa1187b912fd3f0028b3b44742b602dc5
McPizza0 <gh@l1.io>
2024-08-26 12:19:21 +0200

    allow 1 free domain on free plan

--- ee/apps/billing/trpc/routers/iCanHazRouter.ts
+++ ee/apps/billing/trpc/routers/iCanHazRouter.ts
@@ -12,6 +12,7 @@ export const iCanHazRouter = router({
       const { db } = ctx;
 
       const orgId = input.orgId;
+      const allowedDomainsOnFreePlan = 1;
 
       const orgBillingResponse = await db.query.orgBilling.findFirst({
         where: eq(orgBilling.orgId, orgId),
@@ -19,11 +20,22 @@ export const iCanHazRouter = router({
           plan: true
         }
       });
+
+      // if pro, has unlimited domains
       if (orgBillingResponse && orgBillingResponse.plan === 'pro') {
         return true;
       }
 
-      //for skiff users
+      // get existing org domain count
+      const domainQuery = await db.query.domains.findMany({
+        where: and(eq(domains.orgId, orgId), eq(domains.disabled, false)),
+        columns: {
+          id: true
+        }
+      });
+      const existingOrgDomainCount = domainQuery?.length || 0;
+
+      // check if org has bonus domains
       const orgQuery = await db.query.orgs.findFirst({
         where: eq(orgs.id, orgId),
         columns: {
@@ -41,25 +53,22 @@ export const iCanHazRouter = router({
       const orgMetadata = orgQuery.metadata;
 
       // get the bonus where item matches domain
-      const domainBonus = orgMetadata?.bonuses?.find(
+      const domainBonusMetadataEntry = orgMetadata?.bonuses?.find(
         (bonus) => bonus.item === 'domain'
       );
 
-      if (!domainBonus || !('count' in domainBonus.bonus)) {
-        return false;
+      if (
+        domainBonusMetadataEntry &&
+        'count' in domainBonusMetadataEntry.bonus
+      ) {
+        const allowedBonusDomains: number =
+          domainBonusMetadataEntry.bonus.count;
+        if (existingOrgDomainCount < allowedBonusDomains) {
+          return true;
+        }
       }
 
-      const allowedDomains: number = domainBonus.bonus.count;
-
-      const domainQuery = await db.query.domains.findMany({
-        where: and(eq(domains.orgId, orgId), eq(domains.disabled, false)),
-        columns: {
-          id: true
-        }
-      });
-      const domainCount = domainQuery?.length || 0;
-
-      if (domainCount < allowedDomains) {
+      if (existingOrgDomainCount < allowedDomainsOnFreePlan) {
         return true;
       }
 
