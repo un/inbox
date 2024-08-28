9e78205f1ef90bd3759cb45aa46dc1927a5692a9
McPizza0 <gh@l1.io>
2024-08-28 14:25:13 +0200

    change convo list item name

--- apps/web/src/app/[orgShortcode]/convo/_components/convo-list-item.tsx
+++ apps/web/src/app/[orgShortcode]/convo/_components/convo-list-item.tsx
@@ -160,34 +160,21 @@ export const ConvoItem = memo(function ConvoItem({
           )}
           <div className="flex w-[90%] flex-1 flex-col">
             <div className="flex flex-row items-end justify-between gap-1">
-              <span className="truncate text-sm font-medium">
-                {participantNames.join(', ')}
+              <span className="truncate break-all text-left font-medium">
+                {convo.subjects[0]?.subject}
               </span>
               <span className="text-base-11 min-w-fit text-right text-xs">
                 {timeAgo}
               </span>
             </div>
-
-            <span className="truncate break-all text-left text-xs font-medium">
-              {convo.subjects[0]?.subject}
+            <span className="truncate text-xs font-medium">
+              {participantNames.join(', ')}
             </span>
 
             <div className="flex flex-row items-start justify-start gap-1 text-left text-sm">
-              <div className="px-0.5">
-                {authorAvatarData && (
-                  <Avatar
-                    avatarProfilePublicId={
-                      authorAvatarData.avatarProfilePublicId
-                    }
-                    avatarTimestamp={authorAvatarData.avatarTimestamp}
-                    name={authorAvatarData.name}
-                    size={'sm'}
-                    color={authorAvatarData.color}
-                    key={authorAvatarData.participantPublicId}
-                  />
-                )}
-              </div>
-
+              <span className="ph-no-capture font-semibold">
+                {authorAvatarData?.name + ':' ?? ''}
+              </span>
               <span className="ph-no-capture line-clamp-2 overflow-clip break-words">
                 {convo.entries[0]?.bodyPlainText.trim() ?? ''}
               </span>
